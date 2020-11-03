cf07e77906b14caa4839a20c3cfb9ed48744569f
ffangliu <fangliu.ustc@gmail.com>
2020-11-03 11:07:39 -0800

    Modified to use resp charge. Tested on charged system, acetonitrile solvent.
    Committer: Fang Liu <fangliu@comet-ln2.sdsc.edu>
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
           modified:   set_up_solvated_AmberMD.py
           modified:   ../devtools/conda-envs/test_env.yaml
    
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
          modified:   set_up_solvated_AmberMD.py
          modified:   ../devtools/conda-envs/test_env.yaml

--- autosolvate/set_up_solvated_AmberMD.py
+++ autosolvate/set_up_solvated_AmberMD.py
@@ -18,6 +18,7 @@ class solventBoxBuilder():
         self.pbcbox_size = self.cube_size + 1
 
     def getSolutePDB(self):
+        print("Converting xyz to pdb")
 #        cmd = "babel " + self.xyz + " solute.pdb"
 #        subprocess.call(cmd, shell=True)
         obConversion = ob.OBConversion()
@@ -33,11 +34,23 @@ class solventBoxBuilder():
         pdb2.close()
             
     def getFrcmod(self):
-        cmd1 ="$AMBERHOME/bin/antechamber -i solute.xyz.pdb -fi pdb -o solute.mol2 -fo mol2 -c bcc -s 2 -nc "+str(self.slu_netcharge) + " -m " + str(self.slu_spinmult)
+        print("Generate frcmod file for the solute")
+        print("First generate the gaussian input file")
+        cmd1 ="$AMBERHOME/bin/antechamber -i solute.xyz.pdb -fi pdb -o gcrt.com -fo gcrt -gv 1 -ge solute.gesp  -s 2 -nc "+str(self.slu_netcharge) + " -m " + str(self.slu_spinmult)
         print(cmd1)
-        cmd2 ="$AMBERHOME/bin/parmchk2 -i solute.mol2 -f mol2 -o solute.frcmod"
         subprocess.call(cmd1, shell=True)
+        print("Then run Gaussian...")
+        cmd2="export GAUSS_SCRDIR=/scratch/$USER/$SLURM_JOBID ;"
+        cmd2 = cmd2 + "g16 < gcrt.com > gcrt.out"
         subprocess.call(cmd2, shell=True)
+        print("Gaussian ESP calculation done")
+        print("Then write out mol2")
+        cmd3="$AMBERHOME/bin/antechamber -i solute.gesp -fi gesp -o solute.mol2 -fo mol2 -c resp -eq 2 -rn SLU"
+        subprocess.call(cmd3, shell=True)
+        print("Finally generate frcmod with parmchk2")
+        cmd4 ="$AMBERHOME/bin/parmchk2 -i solute.mol2 -f mol2 -o solute.frcmod"
+        subprocess.call(cmd4, shell=True)
+
     def getHeadTail(self):
         pdb = open('solute.mol2').readlines()
         start =0
@@ -74,11 +87,13 @@ class solventBoxBuilder():
         f.close()
     
     def createLib(self):
+        print("Now create the solute library file")
         self.writeTleapcmd1()
         cmd ="tleap -s -f leap.cmd > leap_savelib.log"
         subprocess.call(cmd, shell=True)
 
     def writeTleapcmd_add_water(self):
+        print("Now add water  box to the solute")
         self.getHeadTail()
         f = open("leap_add_solventbox.cmd","w")
         f.write("source leaprc.protein.ff14SB\n")
@@ -87,6 +102,13 @@ class solventBoxBuilder():
         f.write("loadamberparams solute.frcmod\n")
         f.write("mol=loadmol2 solute.mol2\n")
         f.write("check mol\n")
+        if self.slu_netcharge != 0:
+            if self.slu_netcharge > 0:
+                ion = 'Cl-'
+            else:
+                ion = 'Na+'
+            f.write("addIons2 mol " + ion + " 0\n")
+            f.write("check mol\n")
         f.write("solvatebox mol TIP3PBOX " + str(self.waterbox_size) + " iso 0.8  #Solvate the complex with a cubic water box\n")
         f.write("savepdb mol solute_waterbox.pdb\n")
         f.write("saveamberparm mol solute_waterbox.prmtop solute_waterbox.inpcrd\n")
@@ -95,8 +117,8 @@ class solventBoxBuilder():
 
 
     def processPackmolPDB(self):
-        data=open('ch3cn_solvated.pdb').readlines()
-        output=open('processed-ch3cn_solvated.pdb','w')
+        data=open('ch3cn_solvated.packmol.pdb').readlines()
+        output=open('ch3cn_solvated.processed.pdb','w')
         this_resid = 1
         last_resid = 1
         for line in data:
@@ -109,6 +131,7 @@ class solventBoxBuilder():
         output.close()
     
     def packSLUSLV(self):
+        print("Now use packmol to pack the solute and solvent into a box")
         if self.solvent != 'acetonitrile':
             print("The type of solvent is not implemented yet")
             return
@@ -119,7 +142,7 @@ class solventBoxBuilder():
             packmol_inp.write("\n")
             packmol_inp.write("filetype pdb\n")
             packmol_inp.write("\n")
-            packmol_inp.write("output ch3cn_solvated.pdb\n")
+            packmol_inp.write("output ch3cn_solvated.packmol.pdb\n")
             packmol_inp.write("\n")
             packmol_inp.write("# add the solute\n")
             packmol_inp.write("structure solute.pdb\n")
@@ -145,12 +168,13 @@ class solventBoxBuilder():
         f = open("leap_packmol_solvated.cmd","w")
         f.write("source leaprc.protein.ff14SB\n")
         f.write("source leaprc.gaff\n")
+        f.write("source leaprc.water.tip3p\n") # This will load the Ions. Neccessary
         f.write("loadamberparams ch3cn.frcmod\n")
         f.write("loadAmberPrep ch3cn.prep\n")
         f.write("loadamberparams solute.frcmod\n")
         f.write("loadoff solute.lib\n")
         f.write("\n")
-        f.write("SYS = loadpdb processed-ch3cn_solvated.pdb\n")
+        f.write("SYS = loadpdb ch3cn_solvated.processed.pdb\n")
         f.write("check SYS\n")
         f.write("\n")
         if self.slu_netcharge > 0:
@@ -168,11 +192,12 @@ class solventBoxBuilder():
         f.write("set SYS box {" + str(self.pbcbox_size) +", " + str(self.pbcbox_size) + ", " + str(self.pbcbox_size) + "}\n")
         f.write("\n")
         f.write("saveamberparm SYS ch3cn_solvated.prmtop ch3cn_solvated.inpcrd #Save AMBER topology and coordinate files\n")
-        f.write("savepdb SYS ch3cn_solvated.leap.pdb\n")
+        f.write("savepdb SYS ch3cn_solvated.pdb\n")
         f.write("quit\n")
         f.close()
 
     def createAmberParm(self):
+        print("Generate Amber parameters for the solvated system")
         if self.solvent == 'water':
             self.writeTleapcmd_add_water()
             cmd ="tleap -s -f leap_add_solventbox.cmd > leap_add_solventbox.log"
@@ -184,7 +209,7 @@ class solventBoxBuilder():
             subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
-    builder = solventBoxBuilder('coumarin7oh_min.xyz', 'water', slu_netcharge=1, slu_spinmult=2)
+    builder = solventBoxBuilder('coumarin7oh_min.xyz', 'acetonitrile', slu_netcharge=1, slu_spinmult=2)
     builder.getSolutePDB()
     builder.getFrcmod()
     builder.createLib()
