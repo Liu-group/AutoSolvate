dbe22a4902e4bf83ea18b57cc155dfd0bc364012
ffangliu <fangliu.ustc@gmail.com>
2022-01-02 23:34:33 -0500

    Test adding imolecule as dependency to setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    install_requires=['imolecule'],              # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
