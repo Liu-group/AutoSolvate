745ea7cde19354974bb8873fb2061a4704028e53
BrandeisPatrick <59629055+BrandeisPatrick@users.noreply.github.com>
2022-10-18 20:20:45 -0400

    test_example_4.py

---
+++ autosolvate/tests/test_example_4.py
@@ -0,0 +1,36 @@
+#--------------------------------------------------------------------------------------------------#
+# test example 4: Naphthalene Radical in Chloroform: box gen: test pdb, prmtop 
+# author: Patrick Li (2022-10-18) 
+# path: autosolvate/tests/test_case4.py 
+#--------------------------------------------------------------------------------------------------#
+import os
+import pytest
+import numpy as np
+
+import autosolvate
+from . import helper_functions as hp
+
+
+def test_example_4(tmpdir):
+    testName = "test_example_4"
+    
+    autosolvate.startboxgen([
+        "-m", hp.get_input_dir("naphthalene_radical.xyz"),
+        "-s", "chloroform",
+        "-c", "1", 
+        "-u", "2"
+        "-g", "resp", 
+        "-o", "nap_radical_chcl3",
+        ])
+
+    out = "nap_radical_chcl3" 
+    ref = hp.get_reference_dir("nap_radical_chcl3")
+
+    
+    for suffix in [".pdb", ".prmtop", ".inpcrd"]:
+        assert os.path.exists(out + suffix)
+    
+    assert hp.compare_pdb(out, ref)
+    assert hp.compare_inpcrd_prmtop(out, ref)
+
+    
