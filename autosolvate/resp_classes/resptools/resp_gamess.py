445d33ba319a1b8c546f546ca1617251de21961f
Rohit Sai Kiran Gadde <48002154+rohitsaikiran91@users.noreply.github.com>
2024-05-09 18:47:41 -0400

    Update resp_gamess.py
    Adding memory tag to no optimization case

--- autosolvate/resp_classes/resptools/resp_gamess.py
+++ autosolvate/resp_classes/resptools/resp_gamess.py
@@ -137,6 +137,7 @@ class RespGAMESS(RespABC):
      
         # Header for no optimization case.
         gamess_header['singlepoint'] = """\
+ $SYSTEM MWORDS=128 $END   
  $CONTRL SCFTYP={scf} EXETYP=RUN RUNTYP=ENERGY COORD=UNIQUE $END
  $CONTRL ICHARG={charge} MULT={multiplicity} $END
  $CONTRL MOLPLT=.TRUE. UNITS=ANGS NPRINT=8 $END
